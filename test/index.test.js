ae0c5d7c85534f1749c588d56567aa3fba3a7dcb
Connor Turland <connorturland@gmail.com>
2019-11-17 09:23:23 -0500

    add another test

--- test/index.test.js
+++ test/index.test.js
@@ -12,16 +12,39 @@ describe('makeContactable', () => {
       }
     })
   })
+
+  context('when valid types are called before calling init', function () {
+    it('should throw an error', () => {
+      try {
+        makeContactable({ type: 'phone', id: '+12223334444' })
+      } catch (e) {
+        expect(e.toString()).to.equal('Error: init has not been called')
+      }
+    })
+  })
 })
 
-/*
+// uncomment and run for manual testing
 
-      const which = {
-        doTelegram: true
-      }
-      const config = {
-        telegramable: { socketUrl: 'wss://rsf-telegram-bot.herokuapp.com' }
-      }
-      await init(which, config)
-
-*/
+// const which = {
+//   doTelegram: true
+// }
+// const config = {
+//   telegramable: { socketUrl: 'wss://rsf-telegram-bot.herokuapp.com' }
+// }
+// init(which, config).then(() => {
+//   const tg_username = process.env.TEST_TG_USERNAME
+//   console.log('making a contactable type telegram with username: ' + tg_username)
+//   const a = makeContactable({ type: 'telegram', id: tg_username })
+//   console.log('listening for (and logging) all user messages')
+//   a.listen((message) => {
+//     console.log('received message: ' + message)
+//   })
+//   console.log('sending message "hello!" to user')
+//   a.speak('hello!')
+//   console.log('waiting ten seconds to allow manual testing')
+//   console.log('try sending a message back')
+//   setTimeout(async () => {
+//     await shutdown()
+//   }, 10000)
+// })
